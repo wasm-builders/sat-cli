2eea9c9dca602f66554361af40d813b594de4ae8
Philippe Charrière <ph.charriere@gmail.com>
2022-06-06 09:19:38 +0000

    📝 save

--- morgen/target/wasm32-wasi/release/deps/morgen.d
+++
@@ -1,7 +0,0 @@
-/root/runnable/target/wasm32-wasi/release/deps/morgen.wasm: src/lib.rs
-
-/root/runnable/target/wasm32-wasi/release/deps/libmorgen.rlib: src/lib.rs
-
-/root/runnable/target/wasm32-wasi/release/deps/morgen.d: src/lib.rs
-
-src/lib.rs:
--- morgen/target/wasm32-wasi/release/morgen.d
+++
@@ -1,1 +0,0 @@
-/root/runnable/target/wasm32-wasi/release/morgen.wasm: /root/runnable/src/lib.rs
